6f52c4cc2aa09b128f6b7f4be7c2a5cc2d359691
dragon99z <dragon99@online.de>
2024-11-20 23:13:12 +0100

    fixed a bug with the minute calculation

--- libs/updateDCActivity.js
+++ libs/updateDCActivity.js
@@ -4,13 +4,13 @@ var startTime = Date.now();
 module.exports = function updateDCActivity(mcbot, discordbot) {
     setInterval(() => {
         let timeElapsed = (Date.now() - startTime) / 1000;
-
+        
         let hours = Math.floor(timeElapsed / 3600);
         if (hours < 10) {
             hours = "0" + hours;
         }
 
-        let minutes = Math.floor(timeElapsed / 60);
+        let minutes = Math.floor((timeElapsed % 3600) / 60);
         if (minutes < 10) {
             minutes = "0" + minutes;
         }
