670f2e508f1bf50d8691cf4a0a100eb9c0f273b8
dragon99z <dragon99@online.de>
2025-01-07 18:11:29 +0100

    removed the api key requirement for the onlinecheck

--- libs/checkOnline.js
+++ libs/checkOnline.js
@@ -1,8 +1,8 @@
-const axios = require('axios');
+const sendMessage = require("./sendMessage.js")
 
 module.exports = function checkOnline(mcbot, discordbot,api,botid) {
     setInterval(() => {
-      axios.get(`https://api.hypixel.net/status?key=${api}&uuid=${botid}`)
+      /*axios.get(`https://api.hypixel.net/status?key=${api}&uuid=${botid}`)
           .then((response) => {
             // console.log(json);
             if (!response.data.session.online) {
@@ -21,8 +21,24 @@ module.exports = function checkOnline(mcbot, discordbot,api,botid) {
             }
           }).catch(error => {
             console.error('Error fetching hypixel API:', error);
-          });
+          });*/
+
+          if (mcbot.state === 'disconnected') {
+            sendMessage(discordbot, "The bot got disconnected, restarting...");
+            discordbot.user.setActivity({
+              name: "offline",
+              type: ActivityType.Playing,
+            });
+            discordbot.user.setStatus("idle");
+            mcbot = mineflayer.createBot({
+              version: "1.8.9",
+              username: process.env.MICROSOFT_EMAIL,
+              auth: "microsoft",
+              host: "hypixel.net",
+              port: 25565,
+            });
+          }
       console.timeLog("timeElapsed");
-    }, 1000*60*5);
+    }, 1000*30);
   }
 
