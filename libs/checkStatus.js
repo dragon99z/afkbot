1ad583913b96827503a7d86f983755aecd96d55b
dragon99z <dragon99@online.de>
2025-01-26 20:27:16 +0100

    fixed limbo handling

--- libs/checkStatus.js
+++ libs/checkStatus.js
@@ -2,27 +2,32 @@ const sendMessage = require("./sendMessage.js");
 
 module.exports = function checkStatus(mcbot, discordbot,selling) {
     setInterval(() => {
-      if (
-        JSON.stringify(mcbot.scoreboard.sidebar.items).includes(
-          'text":"Your Isla'
-        )
-      ) {
-        console.log("\x1b[32m%s\x1b[0m", "Already on island");
-      } else {
-        mcbot.chat("/locraw");
-        if (!selling) {
-          sendMessage(discordbot, "Not on private island- relocating");
-          console.log("\x1b[32m%s\x1b[0m", "Not on private island- relocating");
-        } else if (selling) {
-          sendMessage(
-            discordbot,
-            "Not on private island- currently selling items"
-          );
-          console.log(
-            "\x1b[32m%s\x1b[0m",
-            "Not on private island- currently selling items"
-          );
+      if (mcbot.scoreboard.sidebar.items != null){
+        if (
+          JSON.stringify(mcbot.scoreboard.sidebar.items).includes(
+            'text":"Your Isla'
+          )
+        ) {
+          console.log("\x1b[32m%s\x1b[0m", "Already on island");
+        } else {
+          mcbot.chat("/locraw");
+          if (!selling) {
+            sendMessage(discordbot, "Not on private island- relocating");
+            console.log("\x1b[32m%s\x1b[0m", "Not on private island- relocating");
+          } else if (selling) {
+            sendMessage(
+              discordbot,
+              "Not on private island- currently selling items"
+            );
+            console.log(
+              "\x1b[32m%s\x1b[0m",
+              "Not on private island- currently selling items"
+            );
+          }
         }
+      }else{
+        mcbot.chat("/locraw");
+        sendMessage(discordbot, "Bot in Limbo - relocating");
       }
     }, 10000);
   }
